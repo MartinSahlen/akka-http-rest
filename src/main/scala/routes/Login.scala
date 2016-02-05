b831b7fc8a625abbe8a8f33a1994ee1ab39fac32
Martin Abelson Sahlen <martin@unacast.com>
2016-02-05 15:49:16 +0100

    Fix some stuff

--- src/main/scala/routes/Login.scala
+++ src/main/scala/routes/Login.scala
@@ -11,27 +11,26 @@ import rejection.Rejection
 import security.Authentication
 
 object Login extends LazyLogging with Directives with JsonSupport  {
-  val route =
-    handleRejections(Rejection.myRejectionHandler) {
-      (pathPrefix("login") & pathEndOrSingleSlash) {
-        post {
-          authenticateBasic[String]("martin", Authentication.basicAuth) { username ⇒
-            logger.info(s"$username successfully logged in!")
-            entity(as[PostRequest]) { request =>
-              com.wix.accord.validate(request) match {
-                case com.wix.accord.Success => complete(PostResponse(s"${request.clientName}"))
-                case f@Failure(_) => {
-                  complete(BadRequest, for {v <- f.violations} yield {
-                    JsObject(Map("error" -> JsString(v.constraint),
-                      "description" -> JsString(v.description.getOrElse("")),
-                      "value" -> JsString(v.value.toString)
-                    ))
-                  })
-                }
+  val route = handleRejections(Rejection.myRejectionHandler) {
+    (pathPrefix("login") & pathEndOrSingleSlash) {
+      post {
+        authenticateBasic[String]("martin", Authentication.basicAuth) { username ⇒
+          logger.info(s"$username successfully logged in!")
+          entity(as[PostRequest]) { request =>
+            com.wix.accord.validate(request) match {
+              case com.wix.accord.Success => complete(PostResponse(s"${request.clientName}"))
+              case f@Failure(_) => {
+                complete(BadRequest, for {v <- f.violations} yield {
+                  JsObject(Map("error" -> JsString(v.constraint),
+                    "description" -> JsString(v.description.getOrElse("")),
+                    "value" -> JsString(v.value.toString)
+                  ))
+                })
               }
             }
           }
         }
       }
     }
+  }
 }
