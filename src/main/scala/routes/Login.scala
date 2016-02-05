8eff4f520a4446a95f6bf1824b381e1df7a009a4
Martin Abelson Sahlen <martin@unacast.com>
2016-02-05 15:53:50 +0100

    Add some more SOC

--- src/main/scala/routes/Login.scala
+++ src/main/scala/routes/Login.scala
@@ -7,26 +7,23 @@ import com.wix.accord.Failure
 import domain.{PostResponse, PostRequest}
 import json.JsonSupport
 import spray.json.{JsString, JsObject}
-import rejection.Rejection
 import security.Authentication
 
 object Login extends LazyLogging with Directives with JsonSupport  {
-  val route = handleRejections(Rejection.myRejectionHandler) {
-    (pathPrefix("login") & pathEndOrSingleSlash) {
-      post {
-        authenticateBasic[String]("martin", Authentication.basicAuth) { username ⇒
-          logger.info(s"$username successfully logged in!")
-          entity(as[PostRequest]) { request =>
-            com.wix.accord.validate(request) match {
-              case com.wix.accord.Success => complete(PostResponse(s"${request.clientName}"))
-              case f@Failure(_) => {
-                complete(BadRequest, for {v <- f.violations} yield {
-                  JsObject(Map("error" -> JsString(v.constraint),
-                    "description" -> JsString(v.description.getOrElse("")),
-                    "value" -> JsString(v.value.toString)
-                  ))
-                })
-              }
+  val route = (pathPrefix("login") & pathEndOrSingleSlash) {
+    post {
+      authenticateBasic[String]("martin", Authentication.basicAuth) { username ⇒
+        logger.info(s"$username successfully logged in!")
+        entity(as[PostRequest]) { request =>
+          com.wix.accord.validate(request) match {
+            case com.wix.accord.Success => complete(PostResponse(s"${request.clientName}"))
+            case f@Failure(_) => {
+              complete(BadRequest, for {v <- f.violations} yield {
+                JsObject(Map("error" -> JsString(v.constraint),
+                  "description" -> JsString(v.description.getOrElse("")),
+                  "value" -> JsString(v.value.toString)
+                ))
+              })
             }
           }
         }
--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.server._
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
 import json.JsonSupport
+import rejection.Rejection
 import routes.Login
 import scala.util.Properties
 
@@ -12,14 +13,15 @@ object Server extends LazyLogging with Directives with JsonSupport with App {
   implicit val actorSystem = ActorSystem("akka-rest-api")
   implicit val materializer = ActorMaterializer()
   implicit val executionContect = actorSystem.dispatcher
-
   val (interface, port) = ("0.0.0.0", Properties.envOrElse("PORT", "8080").toInt)
-  val binding = Http().bindAndHandle(handler = Login.route, interface = interface, port = port)
+
+  var routes = handleRejections(Rejection.myRejectionHandler)(Login.route)
+
+  val binding = Http().bindAndHandle(handler = routes, interface = interface, port = port)
   logger.info(s"Bound to port $port on interface $interface")
   binding onFailure {
     case ex: Exception ⇒
       logger.error(s"Failed to bind to $interface:$port!", ex)
   }
 }
-
 
