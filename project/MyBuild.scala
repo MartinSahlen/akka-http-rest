23bb5cc6ccdd04e33a0905225abc29cbecc02ee2
Martin Abelson Sahlen <martin@unacast.com>
2016-02-05 17:58:32 +0100

    Remove swagger

--- project/MyBuild.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object MyBuild extends Build {
-
-  lazy val root = Project("root", file(".")) dependsOn swaggerProject
-  lazy val swaggerProject = RootProject(uri("https://github.com/Tecsisa/akka-http-swagger.git"))
-
-}
