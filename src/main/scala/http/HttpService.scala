8db89da3badb533c5b0f77ff00c8149cdf3d2e06
Martin Abelson Sahlen <martin@unacast.com>
2016-02-09 20:43:51 +0100

    Add some more config

--- src/main/scala/http/HttpService.scala
+++ src/main/scala/http/HttpService.scala
@@ -1,12 +1,11 @@
 package http
 
 import akka.http.scaladsl.server.Directives
-import json.JsonSupport
 import rejection.Rejection
 import routes.Login
 import utils.CorsSupport
 
-trait HttpService extends JsonSupport with Directives with CorsSupport {
+trait HttpService extends Directives with CorsSupport {
   val routes =
     corsHandler {
       handleRejections(Rejection.myRejectionHandler)(Login.route)
