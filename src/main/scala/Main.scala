6eaae684f88a921b4f64775bfc61a498845c13a3
Martin Abelson Sahlen <Sahlen@Martins-MacBook-Pro-2.local>
2016-02-22 23:25:20 +0100

    Add non-working db but working

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,19 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import com.github.mauricio.async.db.{RowData, QueryResult}
+import com.github.mauricio.async.db.pool.{ConnectionPool, PoolConfiguration}
+import com.github.mauricio.async.db.postgresql.PostgreSQLConnection
+import com.github.mauricio.async.db.postgresql.pool.PostgreSQLConnectionFactory
+import com.github.mauricio.async.db.postgresql.util.URLParser
+import com.github.mauricio.async.db.{Connection, RowData, QueryResult}
 import com.typesafe.scalalogging.LazyLogging
-import db.DB
 import http.HttpService
 import utils.{ Migration, Config }
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-import scala.concurrent.Future
 
-
-object Main extends LazyLogging with HttpService with App with Migration with DB with Config {
+object Main extends LazyLogging with HttpService with App with Migration with Config {
 
   implicit val actorSystem = ActorSystem("akka-rest-api")
   implicit val materializer = ActorMaterializer()
@@ -18,12 +21,26 @@ object Main extends LazyLogging with HttpService with App with Migration with DB
 
   migrate()
 
-  val future: Future[Option[Seq[RowData]]] = fetch("SELECT 0").map {
-    case Some(_) => {
-      print("lol")
-      Some(null)
+  val configuration = URLParser.parse("jdbc:postgresql://localhost:5233/my_database?user=postgres&password=somepassword")
+  val factory = new PostgreSQLConnectionFactory(configuration)
+  val connectionPool = new ConnectionPool(factory, new PoolConfiguration(5, 5, 5))
+  val future: Future[QueryResult] = connectionPool.sendQuery("SELECT 0")
+
+  val mapResult: Future[Any] = future.map(queryResult => queryResult.rows match {
+    case Some(resultSet) => {
+      val row : RowData = resultSet.head
+      row(0)
     }
+    case None => -1
   }
+  )
+
+  val result = Await.result( mapResult, 5 seconds )
+
+  println(result)
+
+  connectionPool.disconnect
+
 
   val (interface, port) = (httpInterface, httpPort)
   val binding = Http().bindAndHandle(handler = allRoutes, interface = interface, port = port)
